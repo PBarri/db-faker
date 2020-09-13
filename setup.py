03133f4e2d76968a73e805f7adea2904e2eb8205
Pablo Barrientos <pablo.barrientos.13@gmail.com>
2020-09-13 13:35:24 +0200

    Edit first draft version

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/PBarri/db_faker',
-    version='0.0.1.dev',
+    version='0.1.0',
     zip_safe=False,
 )
